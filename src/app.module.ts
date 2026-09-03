2642f8bade8284fcd64676cd9da25c467c9376c3
mohamedHassan <mohamed.aboagwa@bayan-tech.com>
2026-09-03 21:11:35 +0300

    readAdmin

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,6 +43,7 @@ const databaseProvider = {
     try {
       await sequelize.authenticate();
       console.log('Database connection has been established successfully.');
+      await sequelize.sync({ force: false });
     } catch (error) {
       console.error('Unable to connect to the database:', error);
     }
